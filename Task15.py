1862a0324a6ed25034413f8a26202547ae4778b9
PetrTomsik <57231063+PetrTomsik@users.noreply.github.com>
2024-12-06 20:16:27 +0100

    Kolekce

--- Task15.py
+++ Task15.py
@@ -9,3 +9,14 @@ print(f"Všechny jmena {jmena}")
 print(f"První jmeno {jmena[0]}")
 print(f"Druhý jmeno {jmena[1]}")
 print(f"Tretí jmeno {jmena[2]}")
+
+
+#druhe reseni
+duha = []
+duha.append(input("Zadej první barvu duhy"))
+duha.append(input("Zadej druhou barvu duhy"))
+duha.append(input("Zadej treti barvu duhy"))
+print(f"Všechny jmena {duha}")
+print(f"První jmeno {duha[0]}")
+print(f"Druhý jmeno {duha[1]}")
+print(f"Tretí jmeno {duha[2]}")
