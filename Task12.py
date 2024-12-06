8c620bca1d2c13cdef475613a6440e48ebc29e9e
PetrTomsik <57231063+PetrTomsik@users.noreply.github.com>
2024-12-06 18:48:50 +0100

    rename Task 11 new Task 12

--- Task12.py
+++ Task12.py
@@ -1,5 +1,7 @@
 text = "qsxht!trfecdtcbgřáýčerdxdpsěš d21rřcřetčěpe**u?rsS"
-delka = len(text)
-print(f"Ignorujte prvních pět znaků: {text[5:delka]}")
-print(f"Otočte řetězec: {text[::-1]}")
-print(f"Vypište každý čtvrtý znak: {text[0:delka:4]}")
+#delka = len(text)
+text1 = text[5::]
+text2 = text1[::-1]
+print(f"Ignorujte prvních pět znaků: {text1}")
+print(f"Otočte řetězec: {text2}")
+print(f"Vypište každý čtvrtý znak: {text2[::4]}")
