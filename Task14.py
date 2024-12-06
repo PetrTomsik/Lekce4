e658ced4958617879b29fb1bda774f20a8fc90c0
PetrTomsik <57231063+PetrTomsik@users.noreply.github.com>
2024-12-06 19:59:45 +0100

    Kolekce

---
+++ Task14.py
@@ -0,0 +1,13 @@
+kolekce_list = ["Zuzana","Xenie"]
+kolekce_list.append("Alena")    # pridaní do kolekce
+print(kolekce_list)
+kolekce_list.append("Bety")
+kolekce_list.append("Cecilie")
+kolekce_list.append("Dana")
+kolekce_list.append("Evžen")
+kolekce_list.append("Filip")
+print(kolekce_list)
+print(f"Prvek na indexu 1: {kolekce_list[1]} ")
+print(f"Prvek na indexu 2: {kolekce_list[2]} ")
+
+print(f'Prvek na indexu 2: {["A","B","C"][1]}')             # hustý
---
+++ Task15.py
@@ -0,0 +1,11 @@
+jmena = []
+text1 = input(f"První prvek")
+text2 = input(f"Druhý prvek")
+text3 = input(f"Třetí prvek")
+jmena.append(text1)
+jmena.append(text2)
+jmena.append(text3)
+print(f"Všechny jmena {jmena}")
+print(f"První jmeno {jmena[0]}")
+print(f"Druhý jmeno {jmena[1]}")
+print(f"Tretí jmeno {jmena[2]}")
